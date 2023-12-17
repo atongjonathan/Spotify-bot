5c3ad90e179acf093f5336f79c35efc2511838ca
JONATHAN ATONG <110544038+atongjonathan@users.noreply.github.com>
2023-12-17 13:24:38 +0300

    feat: --download-ffmpeg

--- functions.py
+++ functions.py
@@ -8,16 +8,21 @@ def download(track_link):
     try:
         # download track
         print("start downloading: " + track_link)
-        normal_download_command = ['spotdl.4-linux', "--bitrate", "320k", track_link] # nomal download
+        normal_download_command = ['spotdl', "--bitrate", "320k", track_link] # nomal download
         command = normal_download_command # normal download
-        subprocess.run(command, cwd="output", stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+        subprocess.run(command, cwd="output")
         print("end downloading: " + track_link)
-        return True
 
     except Exception as e:
         logger.error(e)
+        ffmpeg_command = ['spotdl', "--download-ffmpeg"]
+        subprocess.run(ffmpeg_command)
+        normal_download_command = ['spotdl', "--bitrate", "320k", track_link] # nomal download
+        command = normal_download_command # normal download
+        subprocess.run(command, cwd="output")
+
         # more info: apparently they give "SongError: Track no longer exists"
         # I can handle them more properly later without passing them to this function in the first place
         # e.g. by using spotify a
-        return False
+    return True
 
