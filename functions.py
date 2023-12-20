5611206f1f096c6f52d2d797547d57f223dd4136
JONATHAN ATONG <110544038+atongjonathan@users.noreply.github.com>
2023-12-20 22:09:09 +0300

    fix: subprocess catch error

--- functions.py
+++ functions.py
@@ -5,24 +5,29 @@ logger = getLogger(__name__)
 
 
 def download(track_link):
-    try:
-        # download track
-        print("start downloading: " + track_link)
-        normal_download_command = ['./spotdl', "--bitrate", "320k", track_link] # nomal download
-        command = normal_download_command # normal download
-        subprocess.run(command, cwd="output")
-        print("end downloading: " + track_link)
-
-    except Exception as e:
-        logger.error(e)
-        ffmpeg_command = ['spotdl', "--download-ffmpeg"]
-        subprocess.run(ffmpeg_command)
-        normal_download_command = ['spotdl', "--bitrate", "320k", track_link] # nomal download
-        command = normal_download_command # normal download
-        subprocess.run(command, cwd="output")
-
-        # more info: apparently they give "SongError: Track no longer exists"
-        # I can handle them more properly later without passing them to this function in the first place
-        # e.g. by using spotify a
+  try:
+    # download track
+    print("start downloading: " + track_link)
+    normal_download_command = ['spotdl', "--bitrate", "320k",
+                               track_link]  # nomal download
+    command = normal_download_command  # normal download
+    result = subprocess.run(command,
+                            cwd="output",
+                            check=True,
+                            text=True,
+                            capture_output=True)
+    logger.info(result.stdout)
+    logger.info("end downloading: " + track_link)
     return True
 
+  except subprocess.CalledProcessError as e:
+    logger.error(f"Error executing process {e}")
+    logger.error(f"Outputs {e.output}")
+    # ffmpeg_command = ['spotdl', "--download-ffmpeg"]
+    # subprocess.run(ffmpeg_command)
+    # normal_download_command = ['spotdl', "--bitrate", "320k",
+    #                            track_link]  # nomal download
+    # command = normal_download_command  # normal download
+    # subprocess.run(command, cwd="output")
+
+    return False
