548e57b3154e71b768c844d724d12a2b5e7bcbae
JONATHAN ATONG <110544038+atongjonathan@users.noreply.github.com>
2024-02-01 12:28:12 +0300

    feat: dependencies

--- config.py
+++ config.py
@@ -1,9 +1,15 @@
 import os
-TELEGRAM_BOT_TOKEN = os.getenv('TELEGRAM_BOT_TOKEN')
-SPOTIPY_CLIENT_ID = os.getenv('SPOTIPY_CLIENT_ID')
-SPOTIPY_CLIENT_SECRET = os.getenv('SPOTIPY_CLIENT_SECRET')
-GCS_ENGINE_ID = os.getenv("GCS_ENGINE_ID")
-GCS_API_KEY = os.getenv("GCS_API_KEY")
-MUSICXMATCH_API_KEY = os.getenv("MUSICXMATCH_API_KEY")
-GENIUS_ACCESS_TOKEN = os.getenv('GENIUS_ACCESS_TOKEN')
-YOUTUBE_API_KEY = os.getenv('YOUTUBE_API_KEY')
+from dotenv import load_dotenv
+
+
+load_dotenv("config.env")
+
+TELEGRAM_BOT_TOKEN = os.environ.get('TELEGRAM_BOT_TOKEN')
+SPOTIPY_CLIENT_ID = os.environ.get('SPOTIPY_CLIENT_ID')
+SPOTIPY_CLIENT_SECRET = os.environ.get('SPOTIPY_CLIENT_SECRET')
+GCS_ENGINE_ID = os.environ.get("GCS_ENGINE_ID")
+GCS_API_KEY = os.environ.get("GCS_API_KEY")
+MUSICXMATCH_API_KEY = os.environ.get("MUSICXMATCH_API_KEY")
+GENIUS_ACCESS_TOKEN = os.environ.get('GENIUS_ACCESS_TOKEN')
+YOUTUBE_API_KEY = os.environ.get('YOUTUBE_API_KEY')
+
